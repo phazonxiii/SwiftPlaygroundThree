72bdef5ef514ed5382cfcf36c52a42aac00d3d13
Cliff Bailey <69611676+phazonxiii@users.noreply.github.com>
2022-02-24 11:33:17 -1000

    Completed parts 1-7

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,36 @@
 import UIKit
 
 var str = "Hello, playground"
+
+// Data Types we've used so far: strings, classes, integers
+// How to create a variable that's an array?
+// Declare it with var or let and use this syntax:
+// var someArray = [someType]()
+
+// With integers, you can do this:
+
+// var someInts:[Int] = [10, 20, 30]
+
+var fruitNames: [String]
+
+// How to declare Int array
+// var anIntArray: [Int]
+
+fruitNames = ["Kiwi", "Apples", "Watermelon", "Pumpkin"]
+
+// We put the "" around each fruit's name so that each one is its own string
+
+print(fruitNames)
+
+print("I like to eat\(fruitNames)")
+
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+// Apples would be at Index 0
+
+print(fruitNames[1])
+
+
